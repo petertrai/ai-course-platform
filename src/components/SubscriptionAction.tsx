f82f8470279c1a88d6298b64da09edca6117d931
petertrai <petertrai@gmail.com>
2023-08-22 13:48:27 -0700

    stripe

--- src/components/SubscriptionAction.tsx
+++ src/components/SubscriptionAction.tsx
@@ -1,18 +1,45 @@
-'use client'
+"use client";
 
-import { useSession } from 'next-auth/react'
-import React from 'react'
+import { useSession } from "next-auth/react";
+import React from "react";
+import { Progress } from "./ui/progress";
+import { Button } from "./ui/button";
+import { Zap } from "lucide-react";
+import axios from "axios";
 
-type Props = {}
+type Props = {};
 
 const SubscriptionAction = (props: Props) => {
-
-    const {data} = useSession()
+  const { data } = useSession();
+  const [loading, setLoading] = React.useState(false);
+  const handleSubscribe = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("/api/stripe");
+      // hit stripe endpoint and it returns a url depending on if user is a subscriber or not currently
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log("error", error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <div className='flex flex-col items-center w-1/2 p-4 mx-auto mt-4 rounded-md bg-secondary'>
+    <div className="flex flex-col items-center w-1/2 p-4 mx-auto mt-4 rounded-md bg-secondary">
       {data?.user.credits} / 10 Free Courses
+      <Progress
+        className="mt-2"
+        value={data?.user.credits ? (data.user.credits / 10) * 100 : 0}
+      />
+      <Button
+        disabled={loading}
+        onClick={handleSubscribe}
+        className="mt-3 font-bold text-white transition bg-gradient-to-tr from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600"
+      >
+        Upgrade <Zap className="fill-white ml-2" />
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default SubscriptionAction
+export default SubscriptionAction;
---
+++ src/lib/subscription.ts
@@ -0,0 +1,27 @@
+import { getAuthSession } from "./auth";
+import { prisma } from "./db";
+
+const DAY_IN_MS = 1000 * 60 * 60 * 24;
+
+export const checkSubscription = async () => {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return false;
+  }
+  const userSubscription = await prisma.userSubscription.findUnique({
+    where: {
+      userId: session.user.id,
+    },
+  });
+  if (!userSubscription) {
+    return false;
+  }
+
+  // user sub must have stripe price id and current period end + 1 day is greater than data. now then they have not expired
+  const isValid =
+    userSubscription.stripePriceId &&
+    userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS >
+      Date.now();
+
+  return !!isValid;
+};
