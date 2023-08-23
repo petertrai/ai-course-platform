578af20c0bc2b50c9194c6018b859bd03f897839
petertrai <petertrai@gmail.com>
2023-08-22 18:01:19 -0700

    idk

--- src/components/SubscriptionAction.tsx
+++ src/components/SubscriptionAction.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useSession } from "next-auth/react";
 import React from "react";
 import { Progress } from "./ui/progress";
@@ -16,7 +15,6 @@ const SubscriptionAction = (props: Props) => {
     setLoading(true);
     try {
       const response = await axios.get("/api/stripe");
-      // hit stripe endpoint and it returns a url depending on if user is a subscriber or not currently
       window.location.href = response.data.url;
     } catch (error) {
       console.log("error", error);
@@ -26,17 +24,19 @@ const SubscriptionAction = (props: Props) => {
   };
   return (
     <div className="flex flex-col items-center w-1/2 p-4 mx-auto mt-4 rounded-md bg-secondary">
-      {data?.user.credits} / 10 Free Courses
+      {data?.user.credits} / 10 Free Generations
       <Progress
         className="mt-2"
         value={data?.user.credits ? (data.user.credits / 10) * 100 : 0}
       />
       <Button
+      variant="secondary"
         disabled={loading}
         onClick={handleSubscribe}
         className="mt-3 font-bold text-white transition bg-gradient-to-tr from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600"
       >
-        Upgrade <Zap className="fill-white ml-2" />
+        Upgrade
+        <Zap className="fill-white ml-2" />
       </Button>
     </div>
   );
